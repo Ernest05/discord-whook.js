a6695f95b77d1a65fde975b23dfff079b876eb07
Ernest <47856136+Ernest05@users.noreply.github.com>
2020-02-21 10:48:59 +0100

    Update EmbedBuilder.js

--- src/EmbedBuilder.js
+++ src/EmbedBuilder.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-class EmbedBuilder {
+module.exports = class EmbedBuilder {
     /**
      * @constructor
      */
-    constructor() {
+    constructor () {
         this.embed = [{
             title: null,
             url: null,
@@ -20,89 +20,35 @@ class EmbedBuilder {
     }
 
     /**
+     * Sets the color of the embed.
+     * @param {ColorResolvable} color The color of the embed
+     * @returns {EmbedBuilder} New changes applied to the embed
+     */
+    setColor (color) {
+        if (!color) {
+            throw new Error('You have to provide a color to set!');
+        }
+
+        this.embed[0].color = color;
+
+        return this;
+    }
+
+    /**
      * Sets the title of the embed.
      * @param {string} title The title
-     * @returns {EmbedBuilder}
+     * @returns {EmbedBuilder} New changes applied to the embed
      */
-    setTitle(title) {
-        if(!title) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a title to set a title!");
-        if(title.length > 256) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a title with 256 characters max.!");
-            this.embed[0].title = title;
-            return this;
-    }
-    
-    /**
-     * Sets the URL of the embed.
-     * @param {string}
-     * @returns {EmbedBuilder}
-     */
-    setURL(url) {
-        if(!url) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide an URL to set an URL!");
-            this.embed[0].url = url;
-            return this;
-    }
- 
-    /**
-     * Sets the description of the embed.
-     * @param {string} description The description
-     * @returns {EmbedBuilder}
-     */
-    setDescription(description) {
-        if(!description) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a description to set a description!");
-        if(description.length > 2048) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a description with 2048 characters max.!");
-            this.embed[0].description = description;
-            return this;
-    }
+    setTitle (title) {
+        if (!title) {
+            throw new Error('You have to provide a title to set!');
+        }
+        if (title.length > 256) {
+            throw new Error('The title must have a maximal length of 256 characters.');
+        }
 
-    /**
-     * Sets the image of the embed.
-     * @param {string} imageURL The URL of the image
-     * @returns {EmbedBuilder}
-     */
-    setImage(imageURL) {
-        if(!imageURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a imageURL to set an image!");
-            this.embed[0].image = {
-                url: imageURL
-            };
-            return this;
-    }
+        this.embed[0].title = title;
 
-    /**
-     * Sets the thumbnail of the embed.
-     * @param {string} imageURL The URL of the thumbnail
-     * @returns {EmbedBuilder}
-     */
-    setThumbnail(imageURL) {
-        if(!imageURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a text to set a footer!");
-            this.embed[0].thumbnail = {
-                url: imageURL
-            };
-            return this;
-    }
-
-    /**
-     * Sets the footer of the embed.
-     * @param {string} text The text of the footer
-     * @param {string} iconURL The icon URL of the footer
-     * @returns {EmbedBuilder}
-     */
-    setFooter(text, iconURL) {
-        if(!text) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a text to set a footer!");
-        if(text.length > 2048) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a text of a footer with 2048 characters max.!");
-        if(!iconURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a iconURL to set a footer!");
-            this.embed[0].footer = {
-                text: text,
-                icon_url: iconURL
-            };
-            return this;
-    }
-
-    /**
-     * Sets the timestamp of the embed.
-     * @returns {EmbedBuilder}
-     */
-    setTimestamp() {
-        this.embed[0].timestamp = new Date();
         return this;
     }
 
@@ -110,28 +56,79 @@ class EmbedBuilder {
      * Sets the author of the embed.
      * @param {string} name The name of the author
      * @param {string} iconURL The icon URL of the author
-     * @returns {EmbedBuilder}
+     * @param {string} url The URL of the author
+     * @returns {EmbedBuilder} New changes applied to the embed
      */
-    setAuthor(name, iconURL) {
-        if(!name) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a name to set an author!");
-        if(name.length > 256) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide an author with 256 characters max.!");
-        if(!iconURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a iconURL to set a author!");
-            this.embed[0].author = {
-                name: name,
-                icon_url: iconURL
-            };
-            return this;
+    setAuthor (name, iconURL, url) {
+        if (!name) {
+            throw new Error('You have to provide a author name to set!');
+        }
+        if (name.length > 256) {
+            throw new Error('The author name must have a maximal length of 256 characters.');
+        }
+        if (!iconURL) {
+            throw new Error('You have to provide an imageURL to set into the author.');
+        }
+
+        this.embed[0].author = {
+            name: name,
+            icon_url: iconURL,
+            url: url ? url : null
+        };
+
+        return this;
     }
 
     /**
-     * Sets the color of the embed.
-     * @param {ColorResolvable} color The color of the embed
-     * @returns {EmbedBuilder}
+     * Sets the description of the embed.
+     * @param {string} description The description
+     * @returns {EmbedBuilder} New changes applied to the embed
      */
-    setColor(color) {
-        if(!color) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a color to set a color!");
-            this.embed[0].color = color;
-            return this;
+    setDescription (description) {
+        if(!description) {
+            throw new Error('You have to provide a description to set!');
+        }
+        if (description.length > 2048) {
+            throw new Error('The description must have a maximal length of 2018 characters');
+        }
+
+        this.embed[0].description = description;
+
+        return this;
+    }
+
+    /**
+     * Sets the thumbnail of the embed.
+     * @param {string} imageURL The URL of the thumbnail
+     * @returns {EmbedBuilder} New changes applied to the embed
+     */
+    setThumbnail (imageURL) {
+        if (!imageURL) {
+            throw new Error('You have to provide an imageURL to set a thumbnail!');
+        }
+
+        this.embed[0].thumbnail = {
+            url: imageURL
+        };
+
+        return this;
+    }
+
+    /**
+     * Sets the image of the embed.
+     * @param {string} imageURL The URL of the image
+     * @returns {EmbedBuilder} New changes applied to the embed
+     */
+    setImage (imageURL) {
+        if (!imageURL) {
+            throw new Error('You have to provide an image URL to set an image!');
+        }
+
+        this.embed[0].image = {
+            url: imageURL
+        };
+
+        return this;
     }
 
     /**
@@ -139,21 +136,63 @@ class EmbedBuilder {
      * @param {string} name The name of the field
      * @param {string} value The value of the field
      * @param {boolean} [inline=false] Set the field to display inline
-     * @returns {EmbedBuilder}
+     * @returns {EmbedBuilder} New changes applied to the embed
      */
-    addField(name, value, inline = false) {
-        if(!name) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a name to add a field!");
-        if(name.length > 256) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a name of a field with 256 characters max.!");
-        if(!value) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a value to add a field!");
-        if(value.length > 1024) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a value of a field with 1024 characters max.!");
-        if(this.embed[0].fields.length > 25) throw new Error("[DiscordWebhook-EmbedBuilder] You can include 25 fields max.!");
-            this.embed[0].fields.push({
-                name,
-                value,
-                inline: inline ? true : false
-            });
-            return this;
+    addField (name, value, inline = false) {
+        if (this.embed[0].fields.length > 25) {
+            throw new Error('An embed can\'t have more than 25 fields!');
+        }
+        if(!name) {
+            throw new Error('You have to provide a name to add a field!');
+        }
+        if (name.length > 256) {
+            throw new Error('The field name must have a maximal length of 256 characters');
+        }
+        if (!value) {
+            throw new Error('You have to provide a value to add a field!');
+        }
+        if (value.length > 1024) {
+            throw new Error('The field value must have a maximal length of 1024 characters.');
+        }
+
+        this.embed[0].fields.push({
+            name: name,
+            value: value,
+            inline: inline
+        });
+
+        return this;
+    }
+
+    /**
+     * Sets the timestamp of the embed.
+     * @returns {EmbedBuilder} New changes applied to the embed
+     */
+    setTimestamp () {
+        this.embed[0].timestamp = new Date();
+
+        return this;
+    }
+
+    /**
+     * Sets the footer of the embed.
+     * @param {string} text The text of the footer
+     * @param {string} iconURL The icon URL of the footer
+     * @returns {EmbedBuilder} New changes applied to the embed
+     */
+    setFooter (text, iconURL) {
+        if (!text) {
+            throw new Error('You have to provided a text to set into the footer!');
+        }
+        if (text.length > 2048) {
+            throw new Error('The text footer must have a maximal length of 2048 characters');
+        }
+
+        this.embed[0].footer = {
+            text: text,
+            icon_url: iconURL ? iconURL : null
+        };
+
+        return this;
     }
 };
-
-module.exports = EmbedBuilder;
