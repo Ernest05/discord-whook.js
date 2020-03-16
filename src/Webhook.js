f38b2b64388eff1ff495d326959d8e4a20862e48
Ernest <47856136+Ernest05@users.noreply.github.com>
2020-03-16 15:58:52 +0100

    Update Webhook.js

--- src/Webhook.js
+++ src/Webhook.js
@@ -22,13 +22,14 @@ module.exports = class Webhook {
 
     /**
      * Send a message with a webhook
-     * @param {string} message The message
-     * @param {string} username The webhook username
-     * @param {string} avatarURL The url of the webhook avatar
-     * @param {Array} embed The embed
+     * @param {Object} options The options for the webhook
+     * @param {string} options.content The message content
+     * @param {string} options.username The webhook username
+     * @param {string} options.avatarURL The url of the webhook avatar
+     * @param {Array} options.embed The embed
      * @returns {Promise<Object>} The sent webhook
      */
-    send (message = undefined, username = undefined, avatarURL = undefined, embed = []) {
+    send (options = { content: undefined, username: undefined, avatarURL: undefined, embed: [] }) {
         if (!message && embed.length === 0) {
             throw new Error('Cannot send an empty message!');
         }
@@ -40,13 +41,13 @@ module.exports = class Webhook {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    content: message,
-                    username: username,
-                    avatar_url: avatarURL,
-                    embeds: embed
+                    content: options.message,
+                    username: options.username,
+                    avatar_url: options.avatarURL,
+                    embeds: options.embed
                 })
-            }).catch(err => {
-                throw new Error(`Webhook sending error: ${err}`);
+            }).catch(error => {
+                throw new Error(`Webhook sending error: ${error}`);
             });
         });
     }
